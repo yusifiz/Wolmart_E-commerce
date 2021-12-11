ccbb3b35e17dfaa5c7cf89ff6dc8eb873385ffd6
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-12-12 00:37:13 +0400

    base.html link configuration

--- vendor/urls.py
+++ vendor/urls.py
@@ -4,6 +4,6 @@ from .views import vendor_dokan_store,vendor_wcfm_store
 app_name = 'vendor'
 
 urlpatterns = [
-    path('', vendor_dokan_store, name='vendor'),
-    path('single-vendor/', vendor_wcfm_store, name='single_vendor'),
+    path('', vendor_wcfm_store, name='vendor'),
+    path('single-vendor/', vendor_dokan_store, name='single_vendor'),
 ]
