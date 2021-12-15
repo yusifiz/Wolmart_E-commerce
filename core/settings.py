3cba1a20efaaefa3cd28eb61440b38be55c6cfa4
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-12-15 12:17:07 +0400

    Postgres connection

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'yusif',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
