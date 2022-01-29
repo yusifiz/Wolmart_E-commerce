a0a79ebda292ad387546ea8fbceba683d99f222d
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-01-29 12:50:37 +0400

    Model translations created

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+
+from blog.translation import BlogTranslationOptions
 from . models import Blog
+from modeltranslation.translator import translator, TranslationOptions, register
+
 # Register your models here.
+admin.site.register(Blog)
 
 
+# @admin.register(Blog)
+# class BlogAdmin(admin.ModelAdmin):
+#     fields = ('category', 'name', 'image', 'author', 'content', 'author_comment')
+    
 
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    fields = ('category', 'name', 'image', 'author', 'content', 'author_comment')
+# @register(Blog)
+# class BlogTranslationOptions(TranslationOptions)
