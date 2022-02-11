755eb89ba29f2b519a9b260835f3bd4130969de9
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-11 16:11:05 +0400

    utils folder deleted

--- contact/utils/validators.py
+++
@@ -1,6 +0,0 @@
-
-from django.core.exceptions import ValidationError
-def mail_validator(mail):
-    if not mail.endswith("gmail.com"):
-        raise ValidationError('Daxil edilen mail gmail.com ile bitmelidir')
-    return True
