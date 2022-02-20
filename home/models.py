054444c6083d4e65811eefe0620a49a5128afaef
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-20 23:34:46 +0400

    Subscriber model created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Subscriber(models.Model):
+    email = models.EmailField(max_length=150,blank=True, null=True, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.email:
+            return str(self.email)
+        return str(self.id)
