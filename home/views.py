8086d8b11372ccc9bafa5009f5765057f4a293fc
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-21 18:06:35 +0400

    index view

--- home/views.py
+++ home/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from blog.models import Blog
+from shop.models import Shop
 # Create your views here.
 
 def index(request):
+    product = Shop.objects.filter(price__range=(0, 101))[:2]
+
+
     blog_list = Blog.objects.all()
     context = {
-        'blog_list' : blog_list
+        'blog_list' : blog_list,
+        'product':product
     }
     return render(request, "index.html", context)
