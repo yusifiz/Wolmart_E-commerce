cfc5b6469114127dee8e77e69675a030062b9cc2
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-21 17:36:09 +0400

    product category admin register

--- shop/admin.py
+++ shop/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from . models import Shop, Order, OrderItem
+from . models import Shop, Order, OrderItem, ProductCategory
 # Register your models here.
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
     fields = ('category', 'name', 'image', 'price')
+    
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    fields = ('name',)
     
 admin.site.register(Order)
 admin.site.register(OrderItem)
