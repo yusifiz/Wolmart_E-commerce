075dc3f5d4457344a631571746ea51150ef95ee0
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-21 17:37:17 +0400

    product category context processors added

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,8 +1,16 @@
-from . models import Shop
+from . models import Shop, ProductCategory
 
 def global_shop(request):
     products = Shop.objects.all()
     context = {
         'products':products
+    }
+    return context
+
+
+def global_product_category(request):
+    product_category = ProductCategory.objects.all()
+    context={
+        'product_category':product_category,
     }
     return context
