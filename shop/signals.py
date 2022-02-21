ce27fcf073895d6e5d0e89262f99d6eeb7600f11
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-21 17:34:29 +0400

    product category signal added

--- shop/signals.py
+++ shop/signals.py
@@ -1,10 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from . models import Shop
+from . models import Shop, ProductCategory
 from django.template.defaultfilters import slugify
 
 @receiver(post_save, sender=Shop)
 def save_shop(sender, created, instance, **kwargs):
     if created:
         instance.slug = slugify(instance.name + "-" + str(instance.created_at))
+        instance.save()
+        
+        
+@receiver(post_save, sender=ProductCategory)
+def save_shop(sender, created, instance, **kwargs):
+    if created:
+        instance.slug = slugify(instance.name)
         instance.save()
