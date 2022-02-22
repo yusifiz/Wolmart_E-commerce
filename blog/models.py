cfb1f8532180b91f34bd7f1ae113c473e1409b15
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-22 23:45:49 +0400

    Blog category created

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,18 @@
+from tabnanny import verbose
+from unicodedata import category
 from django.db import models
 from ckeditor.fields import RichTextField
 # Create your models here.
+
+
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=127, null=True, blank=True)
+    slug = models.SlugField(max_length=127, null=True, blank=True)
+
+
+    class Meta:
+        verbose_name = 'Blog Category'
+        verbose_name_plural = 'Blog Categories'
 
 class Blog(models.Model):
     category = models.CharField(max_length=127, blank=True, null=True)
