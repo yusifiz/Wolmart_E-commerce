2c975a5459ccfd4fd9351dcea65a8db2dc24d98c
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-28 00:10:01 +0400

    shop filter html added

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,4 +1,4 @@
-from . models import Shop, ProductCategory
+from . models import Shop, ProductCategory, Color
 
 # def global_shop(request):
 #     products = Shop.objects.all()
@@ -10,7 +10,9 @@ from . models import Shop, ProductCategory
 
 def global_product_category(request):
     product_category = ProductCategory.objects.all()
+    colors = list(Color.objects.all().values_list('id', flat=True))
     context={
         'product_category':product_category,
+        'color':colors
     }
     return context
