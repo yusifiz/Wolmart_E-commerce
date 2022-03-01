7179dc8871d53239826969c5f175aac600548972
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-03-02 00:17:38 +0400

    wishlist js created

---
+++ static/js/wishlist.js
@@ -0,0 +1,41 @@
+let updatebtns = document.getElementsByClassName('wishlist')
+
+for(let i = 0; i < updatebtns.length; i++ ){
+    updatebtns[i].addEventListener('click', function(e){
+        e.preventDefault; 
+        let p = this.dataset.product
+        let a = this.dataset.action
+        console.log('p:', p, 'a:', a) 
+
+        console.log('USER', user)
+
+        if (user === 'AnonymousUser'){
+            console.log('Not logged in')
+
+        }else{
+            updateUserOrder(p, a)
+        }
+    })
+}
+
+function updateUserOrder(p, a){
+    console.log('User logged in, sending data...')
+
+    var url = 'wishlist/'; 
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'X-CSRFToken' : csrftoken,
+        },
+        body: JSON.stringify({'p': p, 'a': a})
+    })
+
+    .then((response) =>{
+        return response.json()
+    })
+    .then((datas) =>{
+        console.log('datas:', datas)
+        location.reload()
+    })
+}
