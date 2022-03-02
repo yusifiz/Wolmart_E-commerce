4786a8cfe4a11202f929ccb8b7c5db556ded0ab4
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-03-03 01:45:05 +0400

    index html top categories section completed

--- home/views.py
+++ home/views.py
@@ -7,11 +7,14 @@ from shop.models import Shop
 def index(request):
     product = Shop.objects.filter(category__name='t-shirt').order_by('-created_at')[:5]
     deals_hot = Shop.objects.filter(price__range=(0, 101)).order_by("-created_at")[:2]
-
+    clothes = Shop.objects.filter(category__name__in=['Clothes', 'Wear','T-shirt'])
+    electronics = Shop.objects.filter(category__name__in=['Electronics', 'Computer','Phone'])
     blog_list = Blog.objects.all()[:5]
     context = {
         'blog_list' : blog_list,
         'product':product,
         'deals_hot':deals_hot,
+        'clothes':clothes,
+        'electronics':electronics,
     }
     return render(request, "index.html", context)
