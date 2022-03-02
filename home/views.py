a51f0dc93845e2dacf487bdedcd134db33e72f81
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-03-03 01:56:22 +0400

    furniture, electronics and clothes section completed

--- home/views.py
+++ home/views.py
@@ -5,16 +5,18 @@ from shop.models import Shop
 # Create your views here.
 
 def index(request):
-    product = Shop.objects.filter(category__name='t-shirt').order_by('-created_at')[:5]
+    # product = Shop.objects.filter(category__name='t-shirt').order_by('-created_at')[:5]
     deals_hot = Shop.objects.filter(price__range=(0, 101)).order_by("-created_at")[:2]
-    clothes = Shop.objects.filter(category__name__in=['Clothes', 'Wear','T-shirt'])
-    electronics = Shop.objects.filter(category__name__in=['Electronics', 'Computer','Phone'])
+    clothes = Shop.objects.filter(category__name__in=['Clothes', 'Wear','T-shirt'])[:5]
+    furniture = Shop.objects.filter(category__name__in=['Furniture', 'Cup'])[:5]
+    electronics = Shop.objects.filter(category__name__in=['Electronics', 'Computer','Phone'])[:5]
     blog_list = Blog.objects.all()[:5]
     context = {
         'blog_list' : blog_list,
-        'product':product,
+        # 'product':product,
         'deals_hot':deals_hot,
         'clothes':clothes,
         'electronics':electronics,
+        'furniture':furniture,
     }
     return render(request, "index.html", context)
