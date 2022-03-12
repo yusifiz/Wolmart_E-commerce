4fb1868b009faa71a7df63c72afe07cf746b6166
yusifiz <yusifosmanov475@gmail.com>
2022-03-13 00:08:06 +0400

    home slider registered to admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
-from .models import Subscriber
+from .models import Subscriber, HomeSlider
 # Register your models here.
 
 admin.site.register(Subscriber)
+admin.site.register(HomeSlider)
